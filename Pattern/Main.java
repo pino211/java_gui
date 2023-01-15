20a4e3ad33d30504b63e8a155cca85d6cdd9e1d3
pino211 <abelgirma299@email.com>
2023-01-15 11:43:34 +0300

    java ex

---
+++ Pattern/Main.java
@@ -0,0 +1,24 @@
+
+import java.util.*;
+class pattern{
+
+}
+public class Main {
+    public static void main(String[] args){
+    string line;
+       int line ,col,row;
+       char ab='A';
+       Scanner abc= new Scanner(System.in);
+        System.out.println(" enter your number");
+        Scanner sr=new Scanner (line.in);
+        for(  row=1;row<=line;row++){
+
+        }
+        for (col=1;col<=row;col++){
+            System.out.println("" +ab);
+        }
+        System.out.println();
+ab++;
+
+}
+}
